b60b9be3e827a258a033ec579d22265fcc3e9476
HydrogenMacro <hydrogenmacro@gmail.com>
2025-06-25 23:55:44 +0000

    do react three fiber stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,64 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { StrictMode, useEffect, useRef, useState, type ReactNode, type Ref, type RefObject } from "react";
+import * as THREE from "three";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { Canvas, useFrame } from "@react-three/fiber";
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [count, setCount] = useState(0);
+    const cameraRef = useRef<THREE.PerspectiveCamera>(null!);
+    return (
+        <StrictMode>
+            <main>
+                <Canvas shadows={true} camera={cameraRef.current!}>
+                    <Camera cameraRef={cameraRef}></Camera>
+                    <ambientLight intensity={Math.PI / 2}></ambientLight>
+                    <pointLight
+                        position={[0, 5, 0]}
+                        intensity={3}
+                        decay={0}
+                    ></pointLight>
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+                    <Box></Box>
+                </Canvas>
+            </main>
+        </StrictMode>
+    );
 }
+function Camera({ cameraRef }: { cameraRef: RefObject<THREE.PerspectiveCamera> }): ReactNode {
+    
+    useEffect(() => {
+        cameraRef!.current.lookAt(new THREE.Vector3(0, 0, 0));
+    }, []);
+    //let [z, setZ] = useState(-10);
+    useFrame((state, delta) => {
+        //z += delta
+    })
+    return (
+        <>
+            <perspectiveCamera
+                ref={cameraRef}
+                position={[10, 0, -10]}
 
-export default App
+            ></perspectiveCamera>
+        </>
+    );
+}
+function Box(): ReactNode {
+    let meshRef = useRef<THREE.Mesh>(null!);
+
+    useFrame((state, delta) => {
+        meshRef.current.rotation.y += delta;
+    });
+
+    return (
+        <>
+            <mesh ref={meshRef} rotation={[0.1, 0.2, 0.3]}>
+                <boxGeometry args={[1, 2, 3]}></boxGeometry>
+                <meshStandardMaterial color="red"></meshStandardMaterial>
+            </mesh>
+        </>
+    );
+}
+export default App;
